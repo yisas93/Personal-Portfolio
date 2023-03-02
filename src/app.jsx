33c067e3fb31a78a0421666b75fea68368ece855
yisas93 <juan249325@gmail.com>
2023-03-02 02:45:35 +0000

    react-router

--- src/app.jsx
+++ src/app.jsx
@@ -4,13 +4,14 @@ import { Header } from './components/header.jsx';
 import { Projects } from './components/projects.jsx';
 import { About}  from './components/about';
 import { motion } from "framer-motion";
+import { Link } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
       <div className="title">
       <h1 className='name'>Juan Carlos Campos</h1>
       <h2>Full Stack Developer</h2></div>
-      <h1 className='about'>About me</h1>
+      <Link to="/aboutme"><h1 className='about'>About me</h1></Link>
       <h1 className='contact'>Contact me</h1>
       <div><About/></div>
       <div><Header/></div>
