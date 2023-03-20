0700065b96cecc5dbc166ee373e5f6dc7bb18a27
yisas93 <juan249325@gmail.com>
2023-03-20 19:06:23 +0000

    react router/strict mode

--- src/index.js
+++ src/index.js
@@ -19,9 +19,9 @@ element:<AboutMe/>}
 ]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+ 
     <RouterProvider router={router} />
-  </React.StrictMode>
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
