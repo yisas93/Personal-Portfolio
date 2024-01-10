d89ba356627e4d7ad514b011f52859082e948b0d
yisas93 <juan249325@gmail.com>
2024-01-10 01:13:56 +0000

    new name

--- src/app.jsx
+++ src/app.jsx
@@ -19,7 +19,7 @@ function App() {
   return (
     <div className="App">
       <div className="title">
-      <h1 className='name'>Juan Carlos Campos</h1>
+      <h1 className='name'>Yisas</h1>
       <h2>Full Stack Developer</h2></div>
       <Link to="/aboutme"><h1 className='about'>About me</h1></Link>
       <h1 className='contact' type='button' data-bs-toggle="modal" data-bs-target="#exampleModal">Contact me</h1>
