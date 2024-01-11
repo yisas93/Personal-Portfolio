2b96a50ee973c73afce3a53b4fdb150a9d1d0438
yisas93 <juan249325@gmail.com>
2024-01-11 10:09:07 -0600

    some changes just to try

--- src/app.jsx
+++ src/app.jsx
@@ -20,26 +20,26 @@ function App() {
     <div className="App">
       <div className="title">
       <h1 className='name'>Yisas</h1>
-      <h2>Full Stack Developer</h2></div>
-      <Link to="/aboutme"><h1 className='about'>About me</h1></Link>
+      <h2>SUP</h2></div>
+      <Link to="/aboutme"><h1 className='about'>under  me</h1></Link>
       <h1 className='contact' type='button' data-bs-toggle="modal" data-bs-target="#exampleModal">Contact me</h1>
  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Contact Info</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Contact Information</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
         <div className="d-grid">
           <div className="d-flex mx-auto info">
             <span><i class="fa-solid fa-envelope"></i></span>
-            <span>juan249325@gmail.com</span>
+            <span>juan249325@gmail.codsafadgdm</span>
             </div>
        
         <div className="d-flex mx-auto info">
         <span><i class="fa-solid fa-phone"></i></span>
-        <span>+506 6047 2692</span>
+        <span>+506 6047 26dsfsdafa2</span>
         </div> 
         </div>
     
